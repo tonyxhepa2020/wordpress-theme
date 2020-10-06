f8e01709ce70b429a6c0e3df43db8ea8163e50b6
Tony Xhepa <tonyxhepa2020@gmail.com>
2020-10-06 11:12:56 +0200

    test

--- wp-content/themes/udemy/includes/custom-nav-walker.php
+++ wp-content/themes/udemy/includes/custom-nav-walker.php
@@ -5,4 +5,23 @@ class Ju_Custom_Nav_Walker extends Walker_Nav_Menu{
     {
         $output .= '<ul class="dropdown-menu"  role="menu">';
     }
+
+    public function start_el( &$output, $item, $depth = 0, $args = [], $id = 0)
+    {
+        $output .= '<li class="item item-title">';
+        $output .= $args->before;
+        $output .= '<a href="' . $item->url . '">';
+        $output .= $args->link_before . $item->title . $args->link_after;
+        $output .= '</a>';
+        $output .= $args->after;
+    }
+    public function end_el(&$output, $item, $depth = 0, $args = null)
+    {
+       $output .= '</li>'; // end the li tag
+    }
+
+    public function end_lvl( &$output, $depth = 0, $args = [])
+    {
+        $output .= '</ul>';
+    }
 }
